8d431b09fbf6db78dbfaed574ed887691cb04989
canidoac <44803784+canidoac@users.noreply.github.com>
2025-04-15 18:51:50 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -52,7 +52,7 @@ const tableauExt = window.tableau.extensions;
 
     async function render(obj) {
         // Check if the object is effectively invisible
-        if (obj.size.width < 100 || obj.size.height < 100 ) {
+        if (obj.size.width < 100 || obj.size.height < 100 || !obj.isVisible) {
             //obj.position.x < -obj.size.width || obj.position.y < -obj.size.height || !obj.isVisible) {
             return; // Skip rendering for invisible objects
         }
